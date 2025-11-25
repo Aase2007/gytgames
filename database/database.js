bf016f5beaa5a9505527463fad41f686210e27dc
kine <kine.sandvik07@gmail.com>
2025-11-25 14:48:29 +0100

    lagt til componetns

--- database/database.js
+++ database/database.js
@@ -3,18 +3,6 @@ const reader = require('xlsx')
 const database = require('./dbconnector.js')
 require('dotenv').config();
  
- 
-// gjør om filen til en json
-const file = reader.readFile('./fuglerliste.xlsx')
-let data = []
-const sheets = file.SheetNames
-for(let i = 0; i < sheets.length; i++){
-   const temp = reader.utils.sheet_to_json(file.Sheets[file.SheetNames[i]])
-   temp.forEach((res) => {
-      data.push(res)
-   })
-}
- 
 async function sendQuery(query){
     try{
         const dbResponse = await database.query(query)
@@ -27,20 +15,6 @@ async function sendQuery(query){
 let createBirds = "CREATE OR REPLACE TABLE birds ( nameNO varchar(255) PRIMARY KEY NOT NULL, nameEN varchar(255), nameLA varchar(255));"
 sendQuery(createBirds)
  
- 
-// putter lista inn i databasen
-async function insertList(){
-    for(let i = 0; i < data.length; i++){
-        let species = data[i]
-        try{
-            let query = `INSERT INTO birds (nameNO, nameEN, nameLA) VALUES ("${species.artNO}", "${species.artEN}", "${species.artLA}");`
-            const dbResponse = await database.query(query)
-        } catch(error){
-            console.error
-        }
-    }
-}
-insertList()
  
 let createObservations = "CREATE OR REPLACE TABLE observations (id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, username VARCHAR(255) NOT NULL, species VARCHAR(255) NOT NULL, place VARCHAR(255) NOT NULL, date DATETIME NOT NULL, description TEXT);"
 sendQuery(createObservations)
